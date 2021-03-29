91bc9d81faa9ac6f905bde603e71adbeeb58cce8
fabiofsilva92 <fabiofsilva92@gmail.com>
2021-03-29 20:37:40 -0300

    Arrumado nó do tema

---
+++ src/controller/Tema.java
@@ -0,0 +1,35 @@
+package controller;
+
+public class Tema {
+	
+	private int idTema;
+	private String nomeTema;
+	private double valorDiaria;
+	
+	//Construtor da lista dinamica de temas
+	public Tema(int idTema, String nomeTema, double valorDiaria) {
+		this.idTema = idTema;
+		this.nomeTema = nomeTema;
+		this.valorDiaria = valorDiaria;
+	}
+
+	//Getters and Setters
+	public int getIdTema() {
+		return idTema;
+	}
+	public void setIdTema(int idTema) {
+		this.idTema = idTema;
+	}
+	public String getNomeTema() {
+		return nomeTema;
+	}
+	public void setNomeTema(String nomeTema) {
+		this.nomeTema = nomeTema;
+	}
+	public double getValorDiaria() {
+		return valorDiaria;
+	}
+	public void setValorDiaria(double valorDiaria) {
+		this.valorDiaria = valorDiaria;
+	}
+}
--- src/controller/nodeTema.java
+++ src/controller/nodeTema.java
@@ -2,39 +2,12 @@ package controller;
 
 public class nodeTema {
 	
-	private int idTema;
-	private String nomeTema;
-	private double valorDiaria;
+	public Tema tema;
 	public nodeTema prox;
 	
 	//Construtor da lista dinamica de temas
-	public nodeTema(int idTema, String nomeTema, double valorDiaria) {
-		this.idTema = idTema;
-		this.nomeTema = nomeTema;
-		this.valorDiaria = valorDiaria;
+	public nodeTema(Tema tema) {
+		this.tema = tema;
 		this.prox = null;
 	}
-
-	//Getters and Setters
-	public int getIdTema() {
-		return idTema;
-	}
-	public void setIdTema(int idTema) {
-		this.idTema = idTema;
-	}
-	public String getNomeTema() {
-		return nomeTema;
-	}
-	public void setNomeTema(String nomeTema) {
-		this.nomeTema = nomeTema;
-	}
-	public double getValorDiaria() {
-		return valorDiaria;
-	}
-	public void setValorDiaria(double valorDiaria) {
-		this.valorDiaria = valorDiaria;
-	}
-
-	
-
 }
